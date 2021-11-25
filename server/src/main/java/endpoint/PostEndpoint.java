887a794f230038d94ca3389792445271df38d8c1
Malo Grall <malo.grall@gmail.com>
2021-11-25 19:39:30 +0100

    Cleared Datastore queries

--- server/src/main/java/endpoint/PostEndpoint.java
+++ server/src/main/java/endpoint/PostEndpoint.java
@@ -4,12 +4,7 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 import com.google.appengine.api.datastore.*;
-import com.google.cloud.datastore.Datastore;
-import com.google.cloud.datastore.DatastoreOptions;
-import com.google.cloud.datastore.QueryResults;
 
-import javax.inject.Named;
-import java.util.ArrayList;
 import java.util.List;
 
 @Api(
@@ -41,30 +36,5 @@ public class PostEndpoint {
         PreparedQuery pq = datastore.prepare(q);
         List<Entity> result = pq.asList(FetchOptions.Builder.withLimit(100));
         return result;
-    }
-
-    @ApiMethod(
-            name = "GQuery",
-            path = "gquery",
-            httpMethod = ApiMethod.HttpMethod.GET
-    )
-    public List<Entity> gQuery(@Named("query") String query) {
-        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-        // [START newQuery]
-        com.google.cloud.datastore.Query<?> glquery = com.google.cloud.datastore.Query.newGqlQueryBuilder(query).build();
-        QueryResults<?> results = datastore.run(glquery);
-
-        List<Entity> list = new ArrayList<>();
-        Entity ent;
-        while(results.hasNext()){
-            ent = new Entity("result");
-            ent.setProperty("result", results.next());
-            list.add(ent);
-        }
-
-        // Use results
-        // [END newQuery]
-
-        return list;
     }
 }
