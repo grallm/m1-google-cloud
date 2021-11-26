e3aa28ee3aa5e9a2f80ef705235f12e90aa550c7
Malo Grall <malo.grall@gmail.com>
2021-11-26 17:59:44 +0100

    Added React Bootstrap + Started header

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,10 +1,21 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from "next-auth/client"
+import Header from '../components/Header'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider session={pageProps.session}>
+      <Header/>
+
       <Component {...pageProps} />
     </Provider>
   )
