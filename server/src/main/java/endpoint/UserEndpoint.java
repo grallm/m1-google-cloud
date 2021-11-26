1fbad72b8dce0add31f50762c57a6615f5a22048
ArKeid0s <matproz.gaming@gmail.com>
2021-11-26 16:58:31 +0100

    Separated Follow and Unfollow

--- server/src/main/java/endpoint/UserEndpoint.java
+++ server/src/main/java/endpoint/UserEndpoint.java
@@ -12,147 +12,151 @@ import entities.UserTiny;
 import java.util.Date;
 import java.util.List;
 
-@Api(
-        name = "instaCrash",
-        version = "v1",
-        // audiences = "616939906371-cnpc0ocrc71ae3hbann3glgiktfgregk.apps.googleusercontent.com",
-        // clientIds = "616939906371-cnpc0ocrc71ae3hbann3glgiktfgregk.apps.googleusercontent.com",
-        namespace =
-        @ApiNamespace(
-                ownerDomain = "tinycrash.ew.r.appspot.com",
-                ownerName = "tinycrash.ew.r.appspot.com",
-                packagePath = ""
-        )
-)
-public class UserEndpoint {
-    /**
-     * Get all Users
-     * @return All Posts
-     */
-    @ApiMethod(
-            name = "getAllUsers",
-            path = "user",
-            httpMethod = ApiMethod.HttpMethod.GET
-    )
-    public List<Entity> getAllUsers() {
-        Query q = new Query("User");
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        PreparedQuery pq = datastore.prepare(q);
-
-        List<Entity> results = pq.asList(FetchOptions.Builder.withLimit(20));
-
-        return results;
-    }
-
-    /**
-     * Add a user
-     * @return Created User
-     */
-    @ApiMethod(
-            name = "addUser",
-            path = "user",
-            httpMethod = ApiMethod.HttpMethod.POST
-    )
-    public Entity addUser(UserTiny userTiny) throws BadRequestException {
-        // Validate given post
-        if (userTiny.email.trim().length() < 4) {
-            throw new BadRequestException("Invalid User");
-        }
-
-        // Add post to Datastore
-        Date now = new Date();
-        Entity e = new Entity("User", userTiny.email);
-        e.setProperty("email", userTiny.email);
-        e.setProperty("name", userTiny.name);
-        e.setProperty("lastConnected", now);
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        Transaction txn = datastore.beginTransaction();
-        datastore.put(e);
-        txn.commit();
-
-        return e;
-    }
-
-    /**
-     * Get a User from its email
-     * @param email email of the User
-     * @return User
-     */
-    @ApiMethod(path = "user/{email}")
-    public Entity getUser(@Named("email") String email) throws EntityNotFoundException {
-        Key postKey = KeyFactory.createKey("User", email);
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-
-        Entity user = datastore.get(postKey);
-
-        return user;
-    }
-
-    /**
-     * Check if a user follows another
-     * @param user email of the User
-     * @param follow email of the User which first one may follow
-     * @return
-     */
-    @ApiMethod(path = "user/{follow}/follow",
-            httpMethod = ApiMethod.HttpMethod.GET
-    )
-    public Entity getFollows(@Named("user") String user, @Named("follow") String follow)
-            throws UnauthorizedException
-    {
-        // Not connected
-        if (user == null) {
-            throw new UnauthorizedException("Invalid credentials");
-        }
-
-        // Check if exists
-        Query q = new Query("Follow")
-                .setFilter(new Query.FilterPredicate("user", Query.FilterOperator.EQUAL, user))
-                .setFilter(new Query.FilterPredicate("follow", Query.FilterOperator.EQUAL, follow));
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        PreparedQuery pq = datastore.prepare(q);
-
-        List<Entity> results = pq.asList(FetchOptions.Builder.withLimit(1));
-
-        // Return null if doesn't follow
-        return results.size() > 0
-                ? results.get(0)
-                : null;
-    }
-
-    /**
-     * Follow a user, giving its email
-     * Unfollow if follow already
-     * @param user email of the User
-     * @param follow email of the User to follow
-     * @return
-     */
-    @ApiMethod(
-            path = "user/{follow}/follow",
-            httpMethod = ApiMethod.HttpMethod.POST
-    )
-    public Entity follow(@Named("user") String user, @Named("follow") String follow)
-            throws EntityNotFoundException, UnauthorizedException
-    {
-        // Not connected
-        if (user == null) {
-            throw new UnauthorizedException("Invalid credentials");
-        }
-
-        // Add post to Datastore
-        Entity e = new Entity("Follow");
-        e.setProperty("user", user);
-        e.setProperty("follow", follow);
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        Transaction txn = datastore.beginTransaction();
-        datastore.put(e);
-        txn.commit();
-
-        return e;
-    }
+@Api(name = "instaCrash", version = "v1",
+		// audiences = "616939906371-cnpc0ocrc71ae3hbann3glgiktfgregk.apps.googleusercontent.com",
+		// clientIds = "616939906371-cnpc0ocrc71ae3hbann3glgiktfgregk.apps.googleusercontent.com",
+		namespace = @ApiNamespace(ownerDomain = "tinycrash.ew.r.appspot.com", ownerName = "tinycrash.ew.r.appspot.com", packagePath = ""))
+public class UserEndpoint
+{
+	/**
+	 * Get all Users
+	 *
+	 * @return All Posts
+	 */
+	@ApiMethod(name = "getAllUsers", path = "user", httpMethod = ApiMethod.HttpMethod.GET)
+	public List<Entity> getAllUsers()
+	{
+		Query q = new Query("User");
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		PreparedQuery pq = datastore.prepare(q);
+		
+		List<Entity> results = pq.asList(FetchOptions.Builder.withLimit(20));
+		
+		return results;
+	}
+	
+	/**
+	 * Add a user
+	 *
+	 * @return Created User
+	 */
+	@ApiMethod(name = "addUser", path = "user", httpMethod = ApiMethod.HttpMethod.POST)
+	public Entity addUser(UserTiny userTiny) throws BadRequestException
+	{
+		// Validate given post
+		if (userTiny.email.trim().length() < 4)
+		{
+			throw new BadRequestException("Invalid User");
+		}
+		
+		// Add post to Datastore
+		Date now = new Date();
+		Entity e = new Entity("User", userTiny.email);
+		e.setProperty("email", userTiny.email);
+		e.setProperty("name", userTiny.name);
+		e.setProperty("lastConnected", now);
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Transaction txn = datastore.beginTransaction();
+		datastore.put(e);
+		txn.commit();
+		
+		return e;
+	}
+	
+	/**
+	 * Get a User from its email
+	 *
+	 * @param email email of the User
+	 * @return User
+	 */
+	@ApiMethod(path = "user/{email}")
+	public Entity getUser(@Named("email") String email) throws EntityNotFoundException
+	{
+		Key postKey = KeyFactory.createKey("User", email);
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		
+		Entity user = datastore.get(postKey);
+		
+		return user;
+	}
+	
+	/**
+	 * Check if a user follows another
+	 *
+	 * @param user   email of the User
+	 * @param follow email of the User which first one may follow
+	 * @return
+	 */
+	@ApiMethod(path = "user/{follow}/follow", httpMethod = ApiMethod.HttpMethod.GET)
+	public Entity getIsFollowing(@Named("user") String user, @Named("follow") String follow) throws UnauthorizedException
+	{
+		// Not connected
+		if (user == null)
+		{
+			throw new UnauthorizedException("Invalid credentials");
+		}
+		
+		// Check if exists
+		Query q = new Query("Follow").setFilter(new Query.FilterPredicate("user", Query.FilterOperator.EQUAL, user)).setFilter(new Query.FilterPredicate("follow", Query.FilterOperator.EQUAL, follow));
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		PreparedQuery pq = datastore.prepare(q);
+		
+		List<Entity> results = pq.asList(FetchOptions.Builder.withLimit(1));
+		
+		// Return null if doesn't follow
+		return results.size() > 0 ? results.get(0) : null;
+	}
+	
+	/**
+	 * Follow a user, giving its email
+	 *
+	 * @param user   email of the User
+	 * @param follow email of the User to follow
+	 * @return
+	 */
+	@ApiMethod(path = "user/{follow}/follow", httpMethod = ApiMethod.HttpMethod.POST)
+	public Entity follow(@Named("user") String user, @Named("follow") String follow) throws EntityNotFoundException, UnauthorizedException
+	{
+		// Not connected
+		if (user == null)
+		{
+			throw new UnauthorizedException("Invalid credentials");
+		}
+		
+		// Add the follow entity to datastore
+		Entity e = new Entity("Follow");
+		e.setProperty("user", user);
+		e.setProperty("follow", follow);
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Transaction txn = datastore.beginTransaction();
+		datastore.put(e);
+		txn.commit();
+		
+		return e;
+	}
+	
+	/**
+	 * Unfollow a user, giving its email
+	 *
+	 * @param user   email of the User
+	 * @param follow email of the User to unfollow
+	 * @return
+	 */
+	@ApiMethod(path = "user/{follow}/unfollow", httpMethod = ApiMethod.HttpMethod.DELETE)
+	public Entity unfollow(@Named("user") String user, @Named("follow") String follow) throws EntityNotFoundException, UnauthorizedException
+	{
+		Entity followingEntity = getIsFollowing(user, follow);
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Transaction txn = datastore.beginTransaction();
+		datastore.delete(txn, followingEntity.getKey());
+		txn.commit();
+		
+		return followingEntity;
+	}
 }
