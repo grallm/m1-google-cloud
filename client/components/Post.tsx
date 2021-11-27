185e87362e0a9cf879dcbaf0d80f6d476a8e0236
Malo Grall <malo.grall@gmail.com>
2021-11-27 12:41:29 +0100

    Client: improved Post + Updated Node and Packages

--- client/components/Post.tsx
+++ client/components/Post.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Button, Card } from 'react-bootstrap'
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
 
 interface Props {
   owner: string
@@ -10,7 +12,7 @@ interface Props {
 const Post: React.FC<Props> = ({ image, owner, description }) => {
   return (
     <Card className="mb-4">
-      <Card.Title className="p-3">{owner}</Card.Title>
+      <Card.Title className="p-3 pb-2">{owner}</Card.Title>
 
       <Image
         src={image}
@@ -19,11 +21,15 @@ const Post: React.FC<Props> = ({ image, owner, description }) => {
         width={100}
         height={100}
         objectFit="cover"
+        className='border-top border-bottom'
       />
 
       <Card.Body>
-        <Card.Text>{description}</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <div className='d-flex mb-1'>
+          <a><FontAwesomeIcon icon={faHeart} style={{ width: '25px', height: '25px' }} /></a>
+          <div className='ms-3'><b>123</b> j'aimes</div>
+        </div>
+        <Card.Text><b>{owner}</b> {description}</Card.Text>
       </Card.Body>
     </Card>
   )
